4733b1f5700b49fd30a33459848455655b7e9454
jizhaoqin <jizhao.qin.de@gmail.com>
2024-06-02 22:16:52 +0200

    docs: demo

---
+++ demo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    eprintln!("Hello, world!");
+    println!("test");
+    let str = "hello".to_string();
+    println!("{}", str);
+    println!("{str}");
+    let array = vec![1, 2, 3, 4, 1];
+    println!("{:?}", &array[..5]);
+}
