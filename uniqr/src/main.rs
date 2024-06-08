169acef34765ec2a4f2e7704c2af8783186d68f0
jizhaoqin <jizhao.qin.de@gmail.com>
2024-06-08 20:40:33 +0200

    build: uniqr, after recording

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use std::fs;
-use std::io::Write;
+use std::{fs, io::Write};
 
 #[derive(Parser)]
 struct Args {
@@ -21,59 +20,58 @@ fn main() {
 }
 
 fn run(args: Args) -> Result<()> {
-    // open input file and handle errors
+    // read input file
     let contents = fs::read_to_string(&args.input_file)
         .map_err(|err| anyhow!("{}: {}: {}", env!("CARGO_PKG_NAME"), args.input_file, err))?;
 
-    // open output file and handle errors, if `args.output_file` is given; otherwise print to `stdout`
-    let mut out_file: Box<dyn Write> = match &args.output_file {
-        Some(out_name) => Box::new(
-            fs::OpenOptions::new()
-                .create(true)
-                .append(true)
-                .open(out_name)
-                .map_err(|err| anyhow!("{}: {}: {}", env!("CARGO_PKG_NAME"), out_name, err))?,
-        ),
+    let mut output_target: Box<dyn Write> = match args.output_file {
         None => Box::new(std::io::stdout()),
+        Some(file_name) => Box::new(
+            fs::File::create(&file_name)
+                .map_err(|err| anyhow!("{}: {}: {}", env!("CARGO_PKG_NAME"), file_name, err))?,
+        ),
     };
 
-    // omit adjacent duplicated lines and count repetition
-    let count_result = count_lines(contents);
+    // logic of count duplicated lines
+    let count_result: Vec<(usize, String)> = count_lines(contents);
 
-    // print `count_result` to the output target
+    // print result
     if args.count {
         for (count, line) in count_result {
-            writeln!(out_file, "{:>7} {:<}", count, line)?;
+            writeln!(output_target, "{:>7} {:<}", count, line)?;
         }
     } else {
         for (_count, line) in count_result {
-            writeln!(out_file, "{:<}", line)?;
+            writeln!(output_target, "{:<}", line)?;
         }
     }
     Ok(())
 }
 
 fn count_lines(contents: String) -> Vec<(usize, String)> {
+    // create temporary variables
     let mut count_result: Vec<(usize, String)> = Vec::new();
-    let mut count = 1;
     let mut previous_line: Option<&str> = None;
+    let mut count: usize = 1;
 
-    for line in contents.lines() {
+    // main logic except the last line
+    for current_line in contents.lines() {
         match previous_line {
-            Some(prev) if prev == line => {
-                count += 1;
-            }
-            Some(prev) => {
-                count_result.push((count, prev.to_string()));
+            None => {}
+            Some(previous) if previous == current_line => count += 1,
+            Some(previous) => {
+                count_result.push((count, previous.to_string()));
                 count = 1;
             }
-            None => {}
         }
-        previous_line = Some(line);
+        previous_line = Some(current_line);
     }
+
+    // take care of the last line
     if let Some(last_line) = previous_line {
         count_result.push((count, last_line.to_string()));
     }
 
+    // return the result
     count_result
 }
